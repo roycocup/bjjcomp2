ef17317836e4a11240af3e2aee8ccfe2a0b7ac3b
Rodrigo Dias <rodrigo.pitta@gmail.com>
2016-01-12 18:27:59 +0000

    implementing for twitter fetching
    http://v2.bjjcomp.dev/twiterTimeline

--- app/Http/Controllers/RestController.php
+++ app/Http/Controllers/RestController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
-use App\API\Twitter\TwitterAPI;
+use TwitterOAuth\Auth\SingleUserAuth;
+use TwitterOAuth\Serializer\ArraySerializer;
+
 
 class RestController extends BaseController
 {
@@ -29,19 +31,63 @@ class RestController extends BaseController
 
 		if ($output) 
 		{
-		    $tweets = json_decode($output,true);
+			$tweets = json_decode($output,true);
 
-		    foreach ($tweets as $tweet)
-		    {
-		        json_encode($tweet);
-		    }
+			foreach ($tweets as $tweet)
+			{
+				json_encode($tweet);
+			}
 		}
 	}
 
 
 	public function testTwitter(){
 		
-		$twitter = new tmhOAuth();
+		$credentials = array(
+			'consumer_key' 			=> env("TWITTER_KEY"),
+			'consumer_secret' 		=> env("TWITTER_SECRET"),
+			'oauth_token' 			=> env("OAUTH_TOKEN"),
+			'oauth_token_secret' 	=> env("OAUTH_SECRET"),
+		);
+
+
+		$auth = new SingleUserAuth($credentials, new ArraySerializer());
+
+		$params = array(
+			'screen_name' => 'LFF4EVER',
+			'count' => 5,
+			'exclude_replies' => true, 
+			'include_rts' => false,
+			'trim_user' => true,
+		);
+
+		$response = $auth->get('statuses/user_timeline', $params);
+
+		// echo '<pre>'; print_r($auth->getHeaders()); echo '</pre>';
+
+		// echo '<pre>'; print_r($response); echo '</pre><hr />';
+
+		// echo '<pre>'; 
+		// foreach($response as $r){
+		// 	print $r['text']."<br>";
+		// }
+		// echo '</pre><hr />';
+
+		$outputArray = [];
+		$i = 0;
+		foreach($response as $r){
+			$outputArray[$i] = [
+				'created_at'=>$r['created_at'], 
+				'text'=>$r['text'],
+			];
+			if (!empty($r['entities']['media'][0]['media_url'])){
+				$outputArray[$i] = array_merge($outputArray[$i], ['media'=>$r['entities']['media'][0]['media_url']]);
+			}
+
+			$i++;
+		}
+		
+		// echo json_encode($outputArray);
 	}
 
 
