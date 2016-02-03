8092b93697d48fae175d1b6bb65e09ad172a7ba2
Rodrigo Dias <rodrigo.pitta@gmail.com>
2016-02-03 20:06:15 +0000

    configurations for gmail

--- config/mail.php
+++ config/mail.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'rodderscode@gmail.com', 'name' => 'LFF Cup Admin'), 'encryption' => 'tls'],
 
     /*
     |--------------------------------------------------------------------------
