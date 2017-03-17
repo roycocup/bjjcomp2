119ae5030e4780b44e1f057f01e4ed5e617d541c
Rodrigo Dias <rodrigo.pitta@gmail.com>
2017-03-17 10:51:15 +0000

    adding a message about automation process again

--- resources/views/payment.blade.php
+++ resources/views/payment.blade.php
@@ -50,7 +50,7 @@
 
 			<div class="img"><strong>Important!</strong><p>To speed up the process of registering, please dont forget
 					to click the return button to RETURN TO MERCHANT after you pay. <br>
-					The automated process should take between 15 minutes to a couple of hours.</p><img src="/img/paypal-comeback.jpg" alt=""></div> <br>
+					The automated process should take between 15 minutes to 24 hours.</p><img src="/img/paypal-comeback.jpg" alt=""></div> <br>
 
 			<?php if ( new DateTime() < App\Helper::eventData("promoUntil")  ): ?>
 			
@@ -99,7 +99,7 @@
 
 			<div class="img"><strong>Important!</strong><p>To speed up the process of registering, please dont forget
 					to click the return button to RETURN TO MERCHANT after you pay. <br>
-					The automated process should take between 15 minutes to a couple of hours.</p><img src="/img/paypal-comeback.jpg" alt=""></div> <br>
+					The automated process should take between 15 minutes to 24 hours.</p><img src="/img/paypal-comeback.jpg" alt=""></div> <br>
 
 			<?php if ( new DateTime() < App\Helper::eventData("promoUntil")  ): ?>
 			<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post" target="_top">
