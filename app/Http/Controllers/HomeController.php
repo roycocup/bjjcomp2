56e2cde4c57fe08296ebc1aa127f702ecc3f94f4
Rodrigo Dias <rodrigo.pitta@gmail.com>
2017-03-18 15:32:15 +0000

    was missing the monolog on homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\MessageBag;
+use Monolog\Logger;
 
 class HomeController extends BaseController
 {
